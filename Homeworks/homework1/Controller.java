3a8242a391952a88a0ed8195715bf6caf6c30495
Vsevolod <vsevolod.lashin@gmail.com>
2022-12-21 14:22:16 +0300

    homework 1 added

---
+++ Homeworks/homework1/Controller.java
@@ -0,0 +1,8 @@
+package Homeworks.homework1;
+
+public class Controller {
+    public static void main(String[] args) {
+        Model model = new Model();
+        model.app();
+    }
+}
---
+++ Homeworks/homework1/View.java
@@ -0,0 +1,9 @@
+package Homeworks.homework1;
+
+public class View {
+    public void info() {
+        System.out.println("Что Вы хотите сделать?");
+        System.out.println("1. Посмотреть базу данных");
+        System.out.println("2. Найти информацию о работнике");
+    }
+}
